70fa0c3d5d30c2a80448585d89346495c229a3bd
Pri3012 <priym2001@gmail.com>
2024-08-10 19:04:55 +0530

    added Lottie animation on home screen app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.airbnb.android:lottie:6.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
